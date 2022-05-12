3e82102e5d6b21822c346e2c56d92d8c14bf736d
Guilherme Maciel de Almeida <guilherme@gmaciel.com>
2022-05-12 09:53:05 -0700

    Add files

---
+++ script.js
@@ -0,0 +1,10 @@
+const params = new URLSearchParams(window.location.search);
+console.log(params);
+//Add event listener for submitting the form
+//Create validations 
+//Loop through params object
+        
+//Use switch for different types of input to get values
+//Append form results in results div
+//Bonus: add styling to the page
+
